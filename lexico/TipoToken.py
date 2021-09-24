345b871bbde74415bbcba5fd395ad1eadf65a6c8
Renan Luiz Babinski <renan10babinski@hotmail.com>
2021-09-24 13:45:55 -0300

    added tokens enumeration

--- lexico/TipoToken.py
+++ lexico/TipoToken.py
@@ -1,1 +1,46 @@
-class TipoToken:
+from enum import Enum
+
+class Token(Enum):
+    # Key Words (Palavras Chave)
+    KwStatements = 0
+    KwAlgorithm = 1
+    KwInteger = 2
+    KwReal = 3
+    KwAssign = 4
+    KwTo = 5
+    KwRead = 6
+    KwPrint = 7
+    KwIf = 8
+    KwThen = 9
+    KwWhile = 10
+    KwBegin = 11
+    KwEnd = 12
+
+    # Operators (Operadores)
+    OpAritMult = 13
+    OpAritDiv = 14
+    OpAritSum = 15 
+    OpAritSub = 16
+    OpRelSmaller = 17
+    OpRelSmallerEqual = 18
+    OpRelBigger = 19
+    OpRelBiggerEqual = 20
+    OpRelEqual = 21
+    OpRelDiff = 22
+    OpBoolAnd = 23
+    OpBoolOr = 24
+
+    # Characters
+    Delim = 25
+    OpenPar = 26
+    ClosePar = 27
+    Var = 28
+    String = 29
+    End = 30
+
+    # Numbers
+    NumInt = 31
+    NumReal = 32
+
+
+
